88185ff7165d953e5b3013660d3033188eecb857
stephancloete <stephancloete@gmail.com>
2022-07-23 13:49:03 +0200

    Interactive Rainbow Challenge Complete
    Completed the Rainbow Challenge according to following instructions:
    
    Challenge
    
    -Create an HTML file with the name rainbow.html
    -Place your script tags before the closing body tag.
    -Write all your JavaScript code within an external file and link it into your document via your script tags.
    -Create a div element with a class of container and set each div's general styling for this class.
    -Create a div for each color in the rainbow.
    -When you load the page all the blocks must be white.
    -When you click on the div it must change to the backround color of its position, e.g. the top must be red.
    -Use JavaScript to set the background color of each div.
    
    Bonus
    
    -Place the color name inside each div, so the red block must have the word 'red' in it when the user clicks the red div.
    -The name of the colour can stay if the color appears.

---
+++ js/rainbow.js
@@ -0,0 +1,28 @@
+function addRedText(){
+    document.getElementById("redBox").style.backgroundColor = "#FF0000";
+    document.getElementById("redText").innerHTML = "<p>Red</p>";
+}
+function addOrangeText(){
+    document.getElementById("orangeBox").style.backgroundColor = "#FF6300";
+    document.getElementById("orangeText").innerHTML = "<p>Orange</p>";
+}
+function addYellowText(){
+    document.getElementById("yellowBox").style.backgroundColor = "#FFFF00";
+    document.getElementById("yellowText").innerHTML = "<p>Yellow</p>";
+}
+function addGreenText(){
+    document.getElementById("greenBox").style.backgroundColor = "#008000";
+    document.getElementById("greenText").innerHTML = "<p>Green</p>";
+}
+function addBlueText(){
+    document.getElementById("blueBox").style.backgroundColor = "#0000FF";
+    document.getElementById("blueText").innerHTML = "<p>Blue</p>";
+}
+function addPurpleText(){
+    document.getElementById("purpleBox").style.backgroundColor = "#4B0082";
+    document.getElementById("purpleText").innerHTML = "<p>Purple</p>";
+}
+function addVioletText(){
+    document.getElementById("violetBox").style.backgroundColor = "#BD31FF";
+    document.getElementById("violetText").innerHTML = "<p>Violet</p>";
+}
